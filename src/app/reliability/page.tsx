4b8da3d27409829501edf77ea16d16b20d4bb985
Vinh Vu <vinhhv@uci.edu>
2024-05-22 12:55:17 -0700

    Add other pages

--- src/app/reliability/page.tsx
+++ src/app/reliability/page.tsx
@@ -1,3 +1,12 @@
+import Hero from "@/components/hero";
+import reliabilityImg from "/public/reliability.jpg";
+
 export default function ReliabilityPage() {
-  return <div>Reliability Page</div>;
+  return (
+    <Hero
+      imgData={reliabilityImg}
+      imgAlt="welding"
+      title="Super high reliability hosting"
+    />
+  );
 }
