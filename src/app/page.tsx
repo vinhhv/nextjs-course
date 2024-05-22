10e001acba1d222e8f6bd2a5fab83bb5e7c569fd
Vinh Vu <vinhhv@uci.edu>
2024-05-22 12:32:57 -0700

    Add hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-import Link from "next/link";
+import Image from "next/image";
+import homeImg from "/public/home.jpg";
 
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div className="absolute -z-10 inset-0">
+      Home Page
+      <Image
+        src={homeImg}
+        alt="car factory"
+        fill
+        style={{ objectFit: "cover" }}
+      />
+    </div>
+  );
 }
