da615cf46f50f047eee3fd302c4cfc62b1b232f1
Vinh Vu <vinhhv@uci.edu>
2024-05-22 11:02:06 -0700

    Add links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <div>
+        <Link href="/performance">Performance</Link>
+        <Link href="/reliability">Reliability</Link>
+        <Link href="/scale">Scale</Link>
+      </div>
+      Home Page
+    </div>
+  );
 }
